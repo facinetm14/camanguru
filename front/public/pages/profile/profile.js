b0e4526066378bcf13cf9cc556a564c1067158e6
facinet.kouyate <facinet.kouyate@neomi.immo>
2024-11-25 17:45:35 +0100

    WIP

--- front/public/pages/profile/profile.js
+++ front/public/pages/profile/profile.js
@@ -1,12 +1,24 @@
 import { userService } from "../../usecases/userService.js";
+import userStore from "../../scripts/userStore.js";
 
 (async () => {
-  const userId = sessionStorage.getItem('userId');
+  const userId = sessionStorage.getItem("userId");
+
   const userInfosResp = await userService.getUserInfos(userId);
+
   if (userInfosResp.ok) {
-    console.log({userInfosResp});
+    const userInfosRespData = await userInfosResp.json();
+    userStore.setState({ user: userInfosRespData });
   }
-  else {
-    console.log('WHat ?');
-  }
+  const user = userStore.getState().user;
+
+  const usernameLabel = document.querySelector(".username-label");
+  usernameLabel.textContent = user.username;
+
+  const profileDropdownMenu = document.querySelector("#profile-dropdownMenu");
+
+  usernameLabel.addEventListener("click", (_event) => {
+    const previous = profileDropdownMenu.style.display;
+    profileDropdownMenu.style.display = previous === "block" ? "none" : "block";
+  });
 })();
